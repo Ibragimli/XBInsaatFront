11948e751f5a311207cf4ed0cddef9370d6e43e2
Elnur Ibrahimli <elnur204@gmail.com>
2023-06-24 19:56:41 +0400

    add all

---
+++ assets/js/localitaztion.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    var dropdown = document.querySelector('.dropdown');
+    dropdown.classList.toggle('active');
+}
+
+function selectLanguage(lang) {
+    // Seçilen dili kullanmak için yapılacak işlemleri burada gerçekleştirin
+    console.log("Seçilen dil: " + lang);
+}
